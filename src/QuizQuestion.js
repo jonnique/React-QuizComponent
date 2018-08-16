0ceb6228b5a2690ef2c697a5630ddc0dcdc64c70
Jonnique <jonnique18@yahoo.com>
2018-08-16 15:43:07 +0800

    Test 6

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,14 +5,26 @@ class QuizQuestion extends Component {
   constructor(props){
     super(props);
 
+    this.state = {
+      incorrectAnswer: false,
+    };
+
     this.handleClick = this.handleClick.bind(this)
   }
 
-  handleClick(buttonText){
-    if(buttonText === this.props.quiz_question.answer){
+  handleClick(buttonText) {
+    if (buttonText === this.props.quiz_question.answer) {
+      this.setState({
+        incorrectAnswer: false
+      });
       this.props.showNextQuestionHandler()
+    }else{
+      this.setState({
+        incorrectAnswer: true
+      });
     }
   }
+
 
   render() {
     return(
@@ -29,6 +41,7 @@ class QuizQuestion extends Component {
             })}
           </ul>
         </section>
+        {this.state.incorrectAnswer ? <p className="error">Sorry, that's not right</p> : null}
       </main>
     )
   }
