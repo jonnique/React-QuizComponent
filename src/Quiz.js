bc9817ee1e0430ef6428cbff0d20c53c01aeb36f
Jonnique <jonnique18@yahoo.com>
2018-08-16 14:01:26 +0800

    Test 3

--- src/Quiz.js
+++ src/Quiz.js
@@ -4,17 +4,17 @@ import QuizQuestion from './QuizQuestion'
 let quizData = require('./quiz_data.json');
 
 class Quiz extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            quiz_position: 1
-        };
+		constructor(props) {
+				super(props);
+				this.state = {
+						quiz_position: 1
+				};
 
-    }
-    render() {
-        return(<div className="QuizQuestion">
-            <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
-        </div>)
-    }
+		}
+		render() {
+				return(<div className="QuizQuestion">
+						<QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+				</div>)
+		}
 }
 export default Quiz;
